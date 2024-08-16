fc50334e5fc992dbd6438a380441b35247e818bc
cyber_sec_vanguard <ouka.lotfi@gmail.com>
2024-08-16 11:12:56 +0100

    Update to pkix_setup.sh
    I tried to compl with Mozilla's modern recommendation for server side security; and with PKCS#8.

--- pkix_setup.sh
+++ pkix_setup.sh
@@ -1,46 +1,113 @@
 #!/bin/bash
+
+
+
 # Initializing directories, the config file, the access controls, the DB, and the serials
 initialize(){
-	echo "Creating directories"
-	mkdir -p pkix/$name/{certs,crl,csr,newcerts,private}
-	echo "done"
-	echo -e "\n--------------------------------------------------------------------\n"	
-	echo "Configuring OpenSSL"	
+	echo "Creating directories..."
+	mkdir -p pkix/$name/{certs,crl,csr,newcerts,private}	
 	cd pkix/$name
-	echo "Setting access controls to priate/"
-	sudo cp /usr/lib/ssl/openssl.cnf ./
-	sed -i "/Where everything is kept/c\\dir 	=	`pwd`	# Where everything is kept" openssl.cnf
+	echo "Setting access controls to priate/..."
 	sudo chmod 700 private
+	echo "Creating directories: DONE"
+	
+	echo -e "\n--------------------------------------------------------------------\n"	
+	
+	echo "Configuring OpenSSL..."
+	sudo cp /usr/lib/ssl/openssl.cnf ./
+	echo "Setting default directory..."
+	sed -i "/Where everything is kept/c\\dir 	=	`pwd`	# Where everything is kept" openssl.cnf
+	echo -e "DONE\nSetting default days..."
+	sed -i "/default_days/c\\default_days    = 90                   # how long to certify for. 90 is recommended by Mozill" openssl.cnf
+	echo -e "DONE\nSetting default hash function..."
+	sed -i "/default_md/c\\default_md      = sha3-512               # use public key SHA3 hash algorithm, with 512-bit output. It is the strongest." openssl.cnf
+	echo -e "DONE\nConfiguring OpenSSL: DONE"
+
+	echo -e "\n--------------------------------------------------------------------\n"
+
 	echo "Creating DB index.txt"
 	touch index.txt
+	echo "DB creation: DONE"
+
+	echo -e "\n--------------------------------------------------------------------\n"
+
 	echo "Creating serial and CRL serial"
 	echo 00 > serial
 	echo 00 > crlnumber
-	echo "Done. The evinronment is ready"
-        echo -e "\n--------------------------------------------------------------------\n" 
-	echo "Creating revokation list..."	
-	# Generating root self-signed certificate
-	gen_root_cert	
-	openssl ca -config openssl.cnf -gencrl -out crl/ca-crl.pem
+	
+	echo -e "\n--------------------------------------------------------------------\n"
+	
+	echo "Evinronment creation: DONE."
+        
+	echo -e "\n--------------------------------------------------------------------\n" 
+	
+	echo "Proceeding to root CA generation..."	
+	gen_root_cert # Calling function to generate root CA cert
+	echo "Root CA generation: DONE"
+
+	echo -e "\n--------------------------------------------------------------------\n"
+	
+	echo "Creating revokation list..."
+	sudo openssl ca -config openssl.cnf -gencrl -out crl/ca-crl.pem # We need root access because we don't have access to the private key.
+	echo "Revokation list creation: DONE"
 }
 
 # A function to generate root self-signed certs
 gen_root_cert(){
-	echo "Generate a private key, in human readable format (thus PEM), and encrypt it using AES with a 256 key. This will promt you to enter a passphrase. You need to remember it, or use a Password Generator such as Proton Pass"
-	openssl genrsa -aes256 -out private/ca-key.pem 4096
-	chmod 400 private/ca-key.pem
-	echo -e "\n--------------------------------------------------------------------\n" 
-	echo "Now, we will create a self-signed cert for our CA, in x509 format (the standardized), that lasts 10 years, with SHA3 256 hash function. This will use your privte key, so it will prompt you for the passphrase"
-	openssl req -config openssl.cnf -key private/ca-key.pem -new -x509 -days 3650 -sha3-256 -out certs/ca.pem
-	chmod 444 certs/ca.pem
-	echo "Done. The cert is now accessible by anyone to read. This way, they can verify your public key, and use it to verify signatures"
+	echo "We now will generate a private Elliptic Curve Digital Signature Algorithm (ECDSA) key, in one of the best curves, in human readable format (called PEM).  This ECDSA key, with this curve, is the one recommended by Mozilla for modern security."
+	key="del-ca-key.pem"
+	#openssl genrsa -aes256 -out private/ca-key.pem 4096 # Gen 4096 bit key
+	openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:secp521r1 -out private/$key
+		# This elliptic curve is said to be backdoored by the NSA.
+	echo "ECDSA key generation: DONE. It is named 'ca-key.pem'"
+	
 	echo -e "\n--------------------------------------------------------------------\n"
-	echo "One last step. We will convert this certificate from the human-readable form, to a computer-readable form (from PEM to DER)"
+		
+	echo "Next, let's change the format to PKCS#8 format, which is the standard. It will prompt it for (1) sudo password, and a passphrase, because we will encrypt the key so that no one can use it; double security! Make sure to memorize it, or use a password manager such as Proton Pass."
+	sudo openssl pkcs8 -topk8 -inform PEM -outform PEM -in private/$key -out private/ca-key.pem -v2 aes256
+	echo "Format changing: DONE"
+
+	echo -e "\n--------------------------------------------------------------------\n"
+	
+	echo "Deleting old, unencrypted private key so that it cannot be recovered..."
+	shred -n 10 -u private/$key # shred will make sure that the file is unrecoverable
+	echo "Old key deletion: DONE"
+	key="ca-key.pem"
+	echo -e "\n--------------------------------------------------------------------\n"
+	
+	echo "Setting access controls to the key..."
+	sudo chmod 400 private/ca-key.pem
+	echo "Setting access controls: DONE. Next: you must set the right owner to the key."
+
+	echo -e "\n--------------------------------------------------------------------\n" 
+	
+	echo "Before we create the certificate, let's set the Certificate Revocation List (CRL) Distribution Point, which is the point you'll use to distribute the CRL. Other's will use it to verify that a cert signed by you is not revoked."
+	echo "Enter the CRL distribution point URI. [i.g., https://crl.example-root-ca.com/ca-crl.pem ]"
+	echo "WARNING: Once it is set, you cannot change it."
+	echo "NOTE: the crl is currently named 'ca-crl.pem', located in crl/"
+	read crldp
+	sed -i "/[ v3_ca ]/a\\crlDistributionPoints = URI:$crldp"
+	
+	echo "Setting CRL Distribution Point: DONE"
+
+	echo -e "\n--------------------------------------------------------------------\n"
+
+	echo "Now, we will create a self-signed cert for our CA, in x509 format (the standardized), that lasts 10 years, with SHA3 256 hash function. This will use your privte key, so it will prompt you for the passphrase"
+	
+	sudo openssl req -config openssl.cnf -key private/$key -new -x509 -days 3650 -sha3-256 -out certs/ca.pem
+	sudo chmod 444 certs/ca.pem
+	
+	echo "Cert creation: DONE. The cert is now accessible by anyone to read. This way, they can verify your public key, and use it to verify signatures"
+	
+	echo -e "\n--------------------------------------------------------------------\n"
+	
+	echo "Convertting the certificate from the human-readable form PEM, to a computer-readable form DER..."
 	openssl x509 -outform der -in certs/ca.pem -out certs/ca.der
+	echo "Cert form conversion: DONE"
+
+        echo -e "\n--------------------------------------------------------------------\n"                     
 
 	echo "Done! Your cert is now ready to be used!"
-	echo -e "\n--------------------------------------------------------------------\n"
-
 }
 
 # The main function
@@ -49,32 +116,40 @@ main(){
 	echo "This script will help you establish and run your local root Certificate Authority"
 	echo -e "\n--------------------------------------------------------------------\n"
 	echo "Installing the single requirement: OpenSSL"
-	#sudo apt update && sudo apt install openssl
+	sudo apt update && sudo apt install openssl
         echo -e "\n--------------------------------------------------------------------\n"
-	echo "Before we proceed, make sure that this script is running in a the location. Everything that we will create will be a sub-directory/sub-files of this directory. Proceed? (yes/no)"
+	echo "Before we proceed, make sure that this script is running in the right location. Everything that we will create will be a sub-directory/sub-files of this directory. Proceed? (yes/no)"
 	read choice
+	
 	while : ; do
-		if [[ $choice != "no" && $choice != "yes" ]] ; then
+		if [[ $choice != "no" && $choice != "yes" ]] ; then # Invalid input
 			echo "Please, enter either 'yes', or 'no'. Keep it simple"
 			read choice
-		elif [ $choice = "no" ] ; then
+	
+		elif [ $choice = "no" ] ; then # Wrong directory
+			echo "Wrong directory, exiting..."
 			exit 0
+	
 		else
-			break
+			break # right directory
 		fi
 	done
 	echo "Greate! Let's keep going"
-        echo -e "\n--------------------------------------------------------------------\n"
+        
+	echo -e "\n--------------------------------------------------------------------\n"
+
 	echo "Choose a name for your local root CA"
 	read name
 	echo -e "Greate name!"
+	
 	echo -e "\n--------------------------------------------------------------------\n"
+	
 	echo "Initializing.."
-	# Initializing
 	initialize $name
 
 	echo "Users must now deliver their CSR to your csr/. From there, you can sign them."
 	echo "Scrip ends here."
+	echo -e "Things to do next\n1. Add profiles to openssl.cnf for extensions (make a backup).\n2. Configure the CRL extension in the profiles."
 }
 
 
